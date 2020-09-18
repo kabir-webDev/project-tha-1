ee531a1fd35bcb26d27e6a30b2f418c705c4a0d7
kabir-webDev <kabir.webdev@gmail.com>
2020-09-18 23:21:27 +0600

    Styling

--- src/component/ChallangeTwo.js
+++ src/component/ChallangeTwo.js
@@ -4,9 +4,12 @@ function ChallangeTwo() {
   const date = new Date().toLocaleDateString();
   const time = new Date().toLocaleTimeString();
   const name = "Kabir";
+  const coolStyle = {
+    color: "teal",
+  };
   return (
     <div>
-      <h1>{`My name is ${name}`}</h1>
+      <h1 style={coolStyle}>{`My name is ${name}`}</h1>
       <p>{`Current Date is: ${date}`}</p>
       <p>{`Current Time is: ${time}`}</p>
     </div>
