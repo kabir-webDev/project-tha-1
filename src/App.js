d1b8d5154fffe3a184e46b06e75a92f87f536fdb
kabir-webDev <kabir.webdev@gmail.com>
2020-09-19 15:25:47 +0600

    time conditional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import ChallangeThree from "./component/ChallangeThree/ChallangeThree";
-import ChallangeTwo from "./component/ChallangeTwo/ChallangeTwo";
+// import ChallangeTwo from "./component/ChallangeTwo/ChallangeTwo";
 
 function App() {
   return (
--- src/component/ChallangeThree/ChallangeThree.js
+++ src/component/ChallangeThree/ChallangeThree.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function ChallangeThree() {
-  return <div>This is challange Three</div>;
+  const time = new Date().getHours();
+  var greeting = "";
+  const styles = {};
+  if (time >= 23 && time <= 6) {
+    greeting = "Good Night";
+  } else if (time > 6 && time <= 10) {
+    greeting = "Good Morning";
+  } else if (time > 10 && time <= 19) {
+    greeting = "Good Day";
+    styles.color = "tomato";
+  } else {
+    greeting = "Good Evening";
+  }
+  return (
+    <div>
+      <h2>Time is : {time}</h2>
+      <h1 style={styles}>{greeting}, sir</h1>
+    </div>
+  );
 }
 
 export default ChallangeThree;
