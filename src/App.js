b2d829c9aeed7e478e126a9a1f11fd4c0601e88a
kabir-webDev <kabir.webdev@gmail.com>
2020-09-20 20:08:57 +0600

    Hooks Chanllenge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import ChallangeThree from "./component/ChallangeThree/ChallangeThree";
+import HookChallange from "./component/HooksChallange/HookChallange";
 // import ChallangeTwo from "./component/ChallangeTwo/ChallangeTwo";
 
 function App() {
   return (
     <div className="App">
       {/* <ChallangeTwo /> */}
-      <ChallangeThree />
+      {/* <ChallangeThree /> */}
+      <HookChallange />
     </div>
   );
 }
---
+++ src/component/HooksChallange/HookChallange.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function HookChallange() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const changeTime = () => {
+    const somoy = new Date().toLocaleTimeString();
+    setTime(somoy);
+  };
+  return (
+    <div>
+      {" "}
+      <br /> <br />
+      <h1>{time}</h1>
+      <button
+        onClick={changeTime}
+        style={{
+          backgroundColor: "tomato",
+          padding: "20px",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          fontSize: "20px",
+        }}
+      >
+        Change Time
+      </button>
+    </div>
+  );
+}
+
+export default HookChallange;
