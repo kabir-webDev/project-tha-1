aa9c72e7a9f5a33827adfd6cca6960ddbf76eb8d
kabir-webDev <kabir.webdev@gmail.com>
2020-09-21 17:26:22 +0600

    Material Button styles

--- src/component/Material/MaterialUI.js
+++ src/component/Material/MaterialUI.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import "./MaterialUI.css";
 
 function MaterialUI() {
   return (
@@ -7,7 +8,7 @@ function MaterialUI() {
       <div className="box">
         <h2>I am material</h2>
         <Button variant="contained" className="btn">
-          Add ON
+          Add To Basket
         </Button>
       </div>
     </div>
