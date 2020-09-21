264ea6d1a4b72c2b6aca3106f009ea3b5434f707
kabir-webDev <kabir.webdev@gmail.com>
2020-09-21 17:09:47 +0600

    Material UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import CD from "./component/CountDown/CD";
 import Form from "./component/FormChallange/Form";
+import MaterialUI from "./component/Material/MaterialUI";
 // import ChallangeThree from "./component/ChallangeThree/ChallangeThree";
 // import HookChallange from "./component/HooksChallange/HookChallange";
 // import ChallangeTwo from "./component/ChallangeTwo/ChallangeTwo";
@@ -12,8 +13,9 @@ function App() {
       {/* <ChallangeTwo />
       <ChallangeThree />
       <HookChallange /> */}
-      <Form />
+      {/* <Form /> */}
       {/* <CD /> */}
+      <MaterialUI />
     </div>
   );
 }
---
+++ src/component/Material/MaterialUI.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MaterialUI() {
+  return <div>I am material</div>;
+}
+
+export default MaterialUI;
