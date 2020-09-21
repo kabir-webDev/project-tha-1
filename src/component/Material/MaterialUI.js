9faceeb131dae1a0a19e9c3a26834dc0187a9d41
kabir-webDev <kabir.webdev@gmail.com>
2020-09-21 17:16:32 +0600

    Material Button

--- src/component/Material/MaterialUI.js
+++ src/component/Material/MaterialUI.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 function MaterialUI() {
-  return <div>I am material</div>;
+  return (
+    <div className="container">
+      <div className="box">
+        <h2>I am material</h2>
+        <Button variant="contained" className="btn">
+          Add ON
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default MaterialUI;
